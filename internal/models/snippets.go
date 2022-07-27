ae45528cae6eb8d1697546bbb0ed8f0a99b3b1f9
Calvin Colton <calvincolton@Calvins-MBP-2.glikhouse.net>
2022-07-27 07:38:07 -0700

    feat: latest snippets

--- internal/models/snippets.go
+++ internal/models/snippets.go
@@ -39,12 +39,13 @@ func (m *SnippetModel) Insert(title string, content string, expires int) (int, e
 
 // Get() returns a specific snippet based on its id
 func (m *SnippetModel) Get(id int) (*Snippet, error) {
-	s := &Snippet{}
-
 	stmt := `SELECT id, title, content, created, expires FROM snippets
 	WHERE expires > UTC_TIMESTAMP() AND id = ?`
 
+	s := &Snippet{}
+
 	row := m.DB.QueryRow(stmt, id)
+
 	err := row.Scan(&s.ID, &s.Title, &s.Content, &s.Created, &s.Expires)
 	if err != nil {
 		if errors.Is(err, sql.ErrNoRows) {
@@ -59,5 +60,31 @@ func (m *SnippetModel) Get(id int) (*Snippet, error) {
 
 // Latest() returns the 10 most recently created snippets
 func (m *SnippetModel) Latest() ([]*Snippet, error) {
-	return nil, nil
+	stmt := `SELECT id, title, content, created, expires FROM snippets 
+	WHERE expires > UTC_TIMESTAMP() ORDER BY id DESC LIMIT 10`
+
+	rows, err := m.DB.Query(stmt)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	snippets := []*Snippet{}
+
+	for rows.Next() {
+		s := &Snippet{}
+
+		err = rows.Scan(&s.ID, &s.Title, &s.Content, &s.Created, &s.Expires)
+		if err != nil {
+			return nil, err
+		}
+
+		snippets = append(snippets, s)
+	}
+
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return snippets, nil
 }
