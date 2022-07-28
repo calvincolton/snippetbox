4d0430d867360148791526a55730a3cb0b238913
Calvin Colton <calvincolton@Calvins-MBP-2.glikhouse.net>
2022-07-27 21:20:16 -0700

    feat: template caching and helper func for rendering templates to reduce code duplication

---
+++ cmd/web/templates.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"html/template"
+	"path/filepath"
+
+	"github.com/calvincolton/snippetbox/internal/models"
+)
+
+type templateData struct {
+	Snippet  *models.Snippet
+	Snippets []*models.Snippet
+}
+
+func newTemplateCache() (map[string]*template.Template, error) {
+	cache := map[string]*template.Template{}
+
+	// Use the filepath.Glob() function to get a slice of all filepaths that
+	// match the pattern "./ui/html/pages/*.tmpl". This will essentially gives
+	// us a slice of all the filepaths for our application 'page' templates
+	// like: [ui/html/pages/home.tmpl ui/html/pages/view.tmpl]
+	pages, err := filepath.Glob("./ui/html/pages/*.tmpl.html")
+	if err != nil {
+		return nil, err
+	}
+
+	// Extract the file name (like 'home.tmpl.html') from the full filepath
+	// and assign it to the name variable.
+	for _, page := range pages {
+		name := filepath.Base(page)
+
+		// Parse the base template file into a template set.
+		ts, err := template.ParseFiles("./ui/html/base.tmpl.html")
+		if err != nil {
+			return nil, err
+		}
+
+		// Call ParseGlob() *on this template set* to add any partials.
+		ts, err = ts.ParseGlob("./ui/html/partials/*.tmpl.html")
+		if err != nil {
+			return nil, err
+		}
+
+		// Call ParseFiles() *on this template set* to add the  page template.
+		ts, err = ts.ParseFiles(page)
+		if err != nil {
+			return nil, err
+		}
+
+		cache[name] = ts
+	}
+
+	return cache, nil
+}
