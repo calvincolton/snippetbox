a65cd6d523c38aaf16d97942f7b1f483e4131f3d
Calvin Colton <calvincolton@Calvins-MBP-2.glikhouse.net>
2022-07-31 10:05:01 -0700

    feat: middleware - recoverPanic, logRequset, secureHeaders, alice middleware chaining

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -1,6 +1,32 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
+
+func (app *application) recoverPanic(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Create a deferred function, which will always be run in the event of a panic as Go unwinds the stack
+		defer func() {
+			// use the builtin recover function to check if there has been a panic or not
+			if err := recover(); err != nil {
+				w.Header().Set("Connection", "close")
+				app.serveError(w, fmt.Errorf("%s", err))
+			}
+		}()
+
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (app *application) logRequest(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		app.infoLog.Printf("%s - %s %s %s", r.RemoteAddr, r.Proto, r.Method, r.URL.RequestURI())
+
+		next.ServeHTTP(w, r)
+	})
+}
 
 func secureHeaders(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
