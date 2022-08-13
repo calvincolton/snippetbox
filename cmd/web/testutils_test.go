86aaa2439107b38ceac8c2ac6f0064230d297168
Calvin Colton <calvincolton@Calvins-MBP-2.hsd1.ca.comcast.net>
2022-08-12 19:00:15 -0700

    feat: tests

--- cmd/web/testutils_test.go
+++ cmd/web/testutils_test.go
@@ -86,7 +86,7 @@ var csrfTokenRX = regexp.MustCompile(`<input type='hidden' name='csrf_token' val
 func extractCSRFToken(t *testing.T, body string) string {
 	matches := csrfTokenRX.FindStringSubmatch(body)
 	// regex match not working for some reason
-	t.Logf("matches: %q\nbody: %q", matches, body)
+	// t.Logf("matches: %q\nbody: %q", matches, body)
 	if len(matches) < 2 {
 		t.Fatal("no csrf token found in body")
 	}
